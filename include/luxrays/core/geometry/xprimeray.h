63c01650a7b8db0dc099b30b0f105d7820e17447
Billy <111899168+AetherTopologist@users.noreply.github.com>
2025-06-13 14:46:00 -0500

    Update xprimeray.h
    Test gamma y axiz curve with beta 2x

--- include/luxrays/core/geometry/xprimeray.h
+++ include/luxrays/core/geometry/xprimeray.h
@@ -41,8 +41,8 @@ public:
 	// Default constructor
 	xPRIMEray()
 		: origin(Point()), direction(Vector(0, 0, 1)),
-		  center(Point()), beta(1.f),
-		  gamma(Vector(1.f, 1.f, 1.f)),
+		  center(Point()), beta(2.f),
+		  gamma(Vector(1.f, 1.4f, 1.f)),
 		  type(xPRIMErayType::POWER), mint(0.0001f), maxt(1e30f) { }
 
 };
